e5bc7f37c7920b071835b412830619a51b408507
niktos <nikitosss121207@gmail.com>
2025-09-30 09:37:29 +0400

    navigate is ready

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { Main } from './mainPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:"/main",
+    element:<Main/>
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
